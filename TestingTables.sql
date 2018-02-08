46f9842fac78a103a699f4e65fcc541a605fe5ca
Arul Palaniappan <ajpalaniappan@gmail.com>
2018-02-07 18:48:45 -0800

    Sample data for client view

--- TestingTables.sql
+++ TestingTables.sql
@@ -3,600 +3,102 @@ Delete From Room;
 Delete From Rewards;
 Delete From Group_packages;
 
-INSERT INTO CUSTOMER VALUES (
-                            0,
-                            'd',
-                            'Dominic',
-                            100
-);
-INSERT INTO CUSTOMER VALUES (
-                            1,
-                            'a',
-                            'Sam',
-                            101
-);
-INSERT INTO CUSTOMER VALUES (
-                            2,
-                            'c',
-                            'Tom',
-                            102
-);
-INSERT INTO CUSTOMER VALUES (
-                            3,
-                            'b',
-                            'Jeffrey',
-                            103
-);
-INSERT INTO CUSTOMER VALUES (
-                            4,
-                            'e',
-                            'Steve',
-                            104
-);
-INSERT INTO CUSTOMER VALUES (
-                            5,
-                            'f',
-                            'Stan',
-                            105
-);
-INSERT INTO CUSTOMER VALUES (
-                            6,
-                            'g',
-                            'Kenny',
-                            106
-);
-INSERT INTO CUSTOMER VALUES (
-                            7,
-                            'h',
-                            'George',
-                            107
-);
-INSERT INTO CUSTOMER VALUES (
-                            8,
-                            'i',
-                            'Jimmy',
-                            108
-);
-INSERT INTO CUSTOMER VALUES (
-                            9,
-                            'j',
-                            'Connor',
-                            109
-);
-
---Rewards
-INSERT INTO REWARDS VALUES (
-                            1,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            2,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            3,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            4,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            0,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            5,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            6,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            7,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            8,
-                            0,
-                            0
-);
-
-INSERT INTO REWARDS VALUES (
-                            9,
-                            0,
-                            0
-);
-
---room
-
-INSERT INTO room VALUES (
-                  1,
-                  'Presidential Suite',
-                  1500
-);
-INSERT INTO room VALUES (
-                  2,
-                  'Presidential Suite',
-                  1500
-);
-INSERT INTO room VALUES (
-                  3,
-                  'Presidential Suite',
-                  1500
-);
-
-INSERT INTO room VALUES (
-                  4,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  5,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  6,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  7,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  8,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  9,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  10,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  11,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  12,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  13,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  14,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  15,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  16,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  17,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  18,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  19,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  20,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  21,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  22,
-                  'Deluxe Suite',
-                  600
-);
-
-INSERT INTO room VALUES (
-                  23,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  24,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  25,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  26,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  27,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  29,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  30,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  31,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  32,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  33,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  34,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  35,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  36,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  37,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  38,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  39,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  40,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  41,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  42,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  43,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  44,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  45,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  46,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  47,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  48,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  49,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  50,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  51,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  52,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  53,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  54,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  55,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  56,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  57,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  58,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  59,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  60,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  61,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  62,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  63,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  64,
-                  'Suite',
-                  250
-);
-
-
-INSERT INTO room VALUES (
-                  65,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  66,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  67,
-                  'Suite',
-                  250
-);
-
-
-
-INSERT INTO room VALUES (
-                  68,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  69,
-                  'Suite',
-                  250
-);
-
-INSERT INTO room VALUES (
-                  70,
-                  'Suite',
-                  250
-);
-INSERT INTO room VALUES (
-                  71,
-                  'Suite',
-                  250
-);
---GP
-
-INSERT INTO GROUP_PACKAGES VALUES (
-                                  'Presidential Suite',
-                                  2,
-                                  35
-);
-
-INSERT INTO GROUP_PACKAGES VALUES (
-                                  'Deluxe Suite',
-                                  3,
-                                  15
-);
-
-INSERT INTO GROUP_PACKAGES VALUES (
-                                  'Suite',
-                                  5,
-                                  9
-);
-
-INSERT INTO GROUP_PACKAGES VALUES (
-                                  'Conference Room',
-                                  2,
-                                  17
-);
-
-INSERT INTO GROUP_PACKAGES VALUES (
-                                  'Ball Room',
-                                  2,
-                                  22
-);
+INSERT INTO CUSTOMER VALUES (0,'d','Dominic',100);
+INSERT INTO CUSTOMER VALUES (1,'a','Sam',101);
+INSERT INTO CUSTOMER VALUES (2,'c','Tom',102);
+INSERT INTO CUSTOMER VALUES (3,'b','Jeffrey',103);
+INSERT INTO CUSTOMER VALUES (4,'e','Steve',104);
+INSERT INTO CUSTOMER VALUES (5,'f','Stan',105);
+INSERT INTO CUSTOMER VALUES (6,'g','Kenny',106);
+INSERT INTO CUSTOMER VALUES (7,'h','George',107);
+INSERT INTO CUSTOMER VALUES (8,'i','Jimmy',108);
+INSERT INTO CUSTOMER VALUES (9,'j','Connor',109);
+
+INSERT INTO REWARDS VALUES (1,0,0);
+INSERT INTO REWARDS VALUES (2,0,0);
+INSERT INTO REWARDS VALUES (3,0,0);
+INSERT INTO REWARDS VALUES (4,0,0);
+INSERT INTO REWARDS VALUES (0,0,0);
+INSERT INTO REWARDS VALUES (5,0,0);
+INSERT INTO REWARDS VALUES (6,0,0);
+INSERT INTO REWARDS VALUES (7,0,0);
+INSERT INTO REWARDS VALUES (8,0,0);
+INSERT INTO REWARDS VALUES (9,0,0);
+
+INSERT INTO room VALUES (1,'Presidential Suite',1500);
+INSERT INTO room VALUES (2,'Presidential Suite',1500);
+INSERT INTO room VALUES (3,'Presidential Suite',1500);
+INSERT INTO room VALUES (4,'Deluxe Suite',600);
+INSERT INTO room VALUES (5,'Deluxe Suite',600);
+INSERT INTO room VALUES (6,'Deluxe Suite',600);
+INSERT INTO room VALUES (7,'Deluxe Suite',600);
+INSERT INTO room VALUES (8,'Deluxe Suite',600);
+INSERT INTO room VALUES (9,'Deluxe Suite',600);
+INSERT INTO room VALUES (10,'Deluxe Suite',600);
+INSERT INTO room VALUES (11,'Deluxe Suite',600);
+INSERT INTO room VALUES (12,'Deluxe Suite',600);
+INSERT INTO room VALUES (13,'Deluxe Suite',600);
+INSERT INTO room VALUES (14,'Deluxe Suite',600);
+INSERT INTO room VALUES (15,'Deluxe Suite',600);
+INSERT INTO room VALUES (16,'Deluxe Suite',600);
+INSERT INTO room VALUES (17,'Deluxe Suite',600);
+INSERT INTO room VALUES (18,'Deluxe Suite',600);
+INSERT INTO room VALUES (19,'Deluxe Suite',600);
+INSERT INTO room VALUES (20,'Deluxe Suite',600);
+INSERT INTO room VALUES (21,'Deluxe Suite',600);
+INSERT INTO room VALUES (22,'Deluxe Suite',600);
+INSERT INTO room VALUES (23,'Suite',250);
+INSERT INTO room VALUES (24,'Suite',250);
+INSERT INTO room VALUES (25,'Suite',250);
+INSERT INTO room VALUES (26,'Suite',250);
+INSERT INTO room VALUES (27,'Suite',250);
+INSERT INTO room VALUES (29,'Suite',250);
+INSERT INTO room VALUES (30,'Suite',250);
+INSERT INTO room VALUES (31,'Suite',250);
+INSERT INTO room VALUES (32,'Suite',250);
+INSERT INTO room VALUES (33,'Suite',250);
+INSERT INTO room VALUES (34,'Suite',250);
+INSERT INTO room VALUES (35,'Suite',250);
+INSERT INTO room VALUES (36,'Suite',250);
+INSERT INTO room VALUES (37,'Suite',250);
+INSERT INTO room VALUES (38,'Suite',250);
+INSERT INTO room VALUES (39,'Suite',250);
+INSERT INTO room VALUES (40,'Suite',250);
+INSERT INTO room VALUES (41,'Suite',250);
+INSERT INTO room VALUES (42,'Suite',250);
+INSERT INTO room VALUES (43,'Suite',250);
+INSERT INTO room VALUES (44,'Suite',250);
+INSERT INTO room VALUES (45,'Suite',250);
+INSERT INTO room VALUES (46,'Suite',250);
+INSERT INTO room VALUES (47,'Suite',250);
+INSERT INTO room VALUES (48,'Suite',250);
+INSERT INTO room VALUES (49,'Suite',250);
+INSERT INTO room VALUES (50,'Suite',250);
+INSERT INTO room VALUES (51,'Suite',250);
+INSERT INTO room VALUES (52,'Suite',250);
+INSERT INTO room VALUES (53,'Suite',250);
+INSERT INTO room VALUES (54,'Suite',250);
+INSERT INTO room VALUES (55,'Suite',250);
+INSERT INTO room VALUES (56,'Suite',250);
+INSERT INTO room VALUES (57,'Suite',250);
+INSERT INTO room VALUES (58,'Suite',250);
+INSERT INTO room VALUES (59,'Suite',250);
+INSERT INTO room VALUES (60,'Suite',250);
+INSERT INTO room VALUES (61,'Suite',250);
+INSERT INTO room VALUES (62,'Suite',250);
+INSERT INTO room VALUES (63,'Suite',250);
+INSERT INTO room VALUES (64,'Suite',250);
+INSERT INTO room VALUES (65,'Suite',250);
+INSERT INTO room VALUES (66,'Suite',250);
+INSERT INTO room VALUES (67,'Suite',250);
+INSERT INTO room VALUES (68,'Suite',250);
+INSERT INTO room VALUES (69,'Suite',250);
+INSERT INTO room VALUES (70,'Suite',250);
+INSERT INTO room VALUES (71,'Suite',250);
+
+
+INSERT INTO GROUP_PACKAGES VALUES ('Presidential Suite',2,35);
+INSERT INTO GROUP_PACKAGES VALUES ('Deluxe Suite',3,15);
+INSERT INTO GROUP_PACKAGES VALUES ('Suite',5,9);
+INSERT INTO GROUP_PACKAGES VALUES ('Conference Room',2,17);
+INSERT INTO GROUP_PACKAGES VALUES ('Ball Room',2,22);
